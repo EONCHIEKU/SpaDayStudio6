0cdbe2a51277d16a67da95db2112fcd5bf0ebcc1
Esther Onchieku <estheronchieku@gmail.com>
2023-03-28 16:06:11 -0500

    Spa Model Studio done

---
+++ SpaDay6/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay6.Models;
+
+namespace SpaDay6.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("/User/Add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("/User")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            // add form submission handling code here
+            ViewBag.Username = newUser.Username;
+            ViewBag.Email = newUser.Email;
+            ViewBag.Date = newUser.Date;
+
+
+                if(newUser.Password == verify)
+            {
+                ViewBag.Username = newUser.Username;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.Error = "Put in the correct password.";
+                return View("Add");
+            }
+
+        }
+    }
+}
---
+++ SpaDay6/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace SpaDay6.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        public DateTime Date;
+        public User()
+        {
+            Date = DateTime.UtcNow;
+        }
+    }
+}
